4f3fb589d6b04e9a835f980507658c13f4e544f8
blackystrngr <kisalk2007@outlook.com>
2025-08-21 07:38:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,23 @@ from telethon import TelegramClient, events
 import asyncio
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-client = TelegramClient("bot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
-@client.on(events.NewMessage)
-async def handler(event):
-    await event.respond("hi there")
-
 def run_bot():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(client.run_until_disconnected())
+
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+    client = TelegramClient("bot", API_ID, API_HASH)
+
+    async def main():
+        await client.start(bot_token=BOT_TOKEN)
+
+        @client.on(events.NewMessage)
+        async def handler(event):
+            await event.respond("hi there")
+
+        await client.run_until_disconnected()
+
+    loop.run_until_complete(main())
